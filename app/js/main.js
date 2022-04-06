f81500ccc5a10a3a20f505e3037a69adad09273e
MikhailPtichkin88 <mikhailptichkin1988@gmail.com>
2022-04-07 02:24:03 +0300

    first page complete, pixel perfect checked

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,41 @@
 $(function () {
-$('.special-offer__slider').slick({
+$('.special-offer__slider').slick();
 
-});
+//перенос кнопок слайдера в другой блок для позиционирования
 $('.special-offer__wrapper').append($('.slick-arrow'));
-$('.product__favorite').on('click', () => {
-  $('.product__favorite').toggleClass('product__favorite--active');
+
+
+//добавить товар в избранное (клик по сердечкам)
+$('.product__favorite').on('click', function() {
+  $(this).toggleClass('product__favorite--active');
 })
 
-var mixer = mixitup('.best__products');
+//счетчик кол-ва товаров
+$('.counter__btn--plus').on('click', function(){
+  let counter = $(this).siblings('.counter__num').text();
+  if (counter < 99) {
+  $(this).siblings('.counter__num').text(++counter);
+  }
+})
+
+$('.counter__btn--minus').on('click', function(){
+  let counter = $(this).siblings('.counter__num').text();
+  if (counter > 0) {
+  $(this).siblings('.counter__num').text(--counter);
+  }
+})
+
+// mixitup для разных блоков
+var containerEl1 = document.querySelector('[data-ref="container-1"]');
+var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+var config = {
+  controls: {
+    scope: 'local'
+  }
+};
+
+var mixer1 = mixitup(containerEl1, config);
+var mixer2 = mixitup(containerEl2, config);
+
 })
